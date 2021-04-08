af430b0aeb27f9eca8e2baeeaad890a7b35df53c
azamat <azamat@mail.uz>
2021-04-08 09:58:49 +0500

    iOS version example

---
+++ iOS/BankApp/SceneDelegate.swift
@@ -0,0 +1,69 @@
+//
+//  SceneDelegate.swift
+//  BankApp
+//
+//  Created by Azamat on 07/04/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            
+            let components = URLComponents(url: url, resolvingAgainstBaseURL: false)
+            let code = components?.queryItems?.first(where: { $0.name == "code" })?.value
+            
+            let url = URL(string: "http://my2.dev.gov.uz/azamat/ru/test/get-user?code=" + code!)!
+            
+            URLSession.shared.dataTask(with: url) {(data, response, error) in
+                guard let data = data else { return }
+                print(String(data: data, encoding: .utf8)!)
+            }.resume()
+            
+            let alert = UIAlertController(title: "Result", message: "For result look debug output!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.window?.rootViewController?.present(alert, animated: true, completion: nil)
+        }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+    }
+}
+
---
+++ iOS/BankApp/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  BankApp
+//
+//  Created by Azamat on 07/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var authTypeSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func login(_ sender: Any) {
+        var urlComps = URLComponents(string: "https://myid.uz/api/v1/oauth2/authorization")!
+        let queryItems = [
+            URLQueryItem(name: "client_id", value: YOU_CLINET_ID),
+            URLQueryItem(name: "redirect_uri", value: "uzinfocom://bank"),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "method", value: authTypeSwitch.isOn ? "strong" : "simple"),
+        ]
+        
+        urlComps.queryItems = queryItems
+        
+        UIApplication.shared.open(urlComps.url!)
+    }
+}
+
